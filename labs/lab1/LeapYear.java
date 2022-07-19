19f9bae13b40d8d039bf19e3bbb32a1d326e0d19
CJH <875121393@qq.com>
2022-07-19 13:45:51 +0800

    completed lab1

--- labs/lab1/LeapYear.java
+++ labs/lab1/LeapYear.java
@@ -1,8 +1,7 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author JianhuiChen
  */
 public class LeapYear {
-
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
@@ -12,6 +11,12 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+    public static boolean isLeapYear(int year) {
+        /** Check whether a year is leapyear
+         * @param year to be checked 
+         */
+        return (year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
